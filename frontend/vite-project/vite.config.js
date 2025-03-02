f83b001a986ed947d4f948810429fecadfea42f9
shuklagabhishek <shuklagabhishek@gmail.com>
2025-03-02 20:14:10 +0530

    vite config js

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: true, // Ensures Vite runs on the correct network
+  },
+
+   preview: {
+    host: true,
+    allowedHosts: ['aethermind-production-413d.up.railway.app'], // Allow Railway host
+  },
 })
